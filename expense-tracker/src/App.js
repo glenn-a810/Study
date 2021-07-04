703c9aca123614a0e6138ee03facab90203ff34d
Glenn <a810.glenn@gmail.com>
2021-07-04 23:19:00 +0900

    regular JavaScript 구동식 복습

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,7 +1,13 @@
 function App() {
+  // regular JavaScript, cumbersome
+  // const para = document.createElement('p')
+  // para.textContent = 'This is also visible'
+  // document.getElementById('root').append(para)
+
   return(
     <div>
       <h2>Expense Tracker WebApp</h2>
+      <p>This is also visible</p>
     </div>
   )
 }
