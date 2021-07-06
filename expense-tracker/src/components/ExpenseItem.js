f3ef6b6c770a956d8f0e48cd2d7259966b079bda
Glenn <a810.glenn@gmail.com>
2021-07-06 23:26:47 +0900

    basic CSS styling

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
     return(
-    <div>
-        <div>2021년 07월 06일</div>
-        <div>
-            <h2>자동차 보험</h2>
-            <div>294,670 원</div>
+        <div className='expense-item'>
+            <div>2021년 07월 06일</div>
+            <div className='expense-item__description'>
+                <h2>자동차 보험</h2>
+                <div className='expense-item__price'>294,670 원</div>
+            </div>
         </div>
-    </div>
     )
 }
 
