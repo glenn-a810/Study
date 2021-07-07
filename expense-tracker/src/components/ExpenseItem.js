f42f25829389f9bff3a66262805c4a548693a7df
Glenn <a810.glenn@gmail.com>
2021-07-07 23:24:06 +0900

    props 기초 다시보기

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,6 +1,6 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
     const expenseDate = new Date(2021, 6, 6)
     const expenseTitle = '자동차 보험'
     const expenseAmount = 294670
@@ -9,8 +9,8 @@ function ExpenseItem() {
         <div className='expense-item'>
             <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>{expenseAmount} 원</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount} 원</div>
             </div>
         </div>
     )
