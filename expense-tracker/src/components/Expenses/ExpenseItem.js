6a7e18dfcc5175f41f86aa4912bca592f8cbb120
Glenn <a810.glenn@gmail.com>
2021-07-14 22:40:06 +0900

    event handler 테스트

--- expense-tracker/src/components/Expenses/ExpenseItem.js
+++ expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,9 @@ import Card from '../UI/Card'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
+    const clickHandler = () => {
+        console.log('로그 출력테스트')
+    }
 
     return(
         <Card className='expense-item'>
@@ -12,6 +15,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount} 원</div>
             </div>
+            <button onClick={clickHandler}>수정</button>
         </Card>
     )
 }
