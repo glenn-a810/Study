5dbc8ec478f7bf5a314bef71ba8e939cb4214bfc
Glenn <a810.glenn@gmail.com>
2021-07-15 21:51:59 +0900

    state를 사용하지 않을 경우 동작상태 예시

--- expense-tracker/src/components/Expenses/ExpenseItem.js
+++ expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -4,15 +4,18 @@ import Card from '../UI/Card'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
+
+    let title = props.title
+
     const clickHandler = () => {
-        console.log('로그 출력테스트')
+        title = 'Updated'
     }
 
     return(
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>{props.amount} 원</div>
             </div>
             <button onClick={clickHandler}>수정</button>
