8e2917145f9c4d8ff37e2e71891665ddf3797d1a
Glenn <a810.glenn@gmail.com>
2021-07-20 23:02:16 +0900

    multiple state 복습

--- expense-tracker/src/components/Form/ExpenseForm.js
+++ expense-tracker/src/components/Form/ExpenseForm.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredAmount, setEnteredAmount] = useState('')
+    const [enteredDate, setEnteredDate] = useState('')
+
     const titleChangeHandler = (event) => {
-        console.log(event.target.value)
+        // console.log(event.target.value)
+        setEnteredTitle(event.target.value)
+    }
+
+    const amountChangeHandler = event => {
+        setEnteredAmount(event.target.value)
+    }
+
+    const dateChangeHandler = event => {
+        setEnteredDate(event.target.value)
     }
 
     return(
@@ -15,11 +28,11 @@ const ExpenseForm = () => {
                 </div>
                 <div className='new-expense__control'>
                     {/* <label>비용</label> */}
-                    <input type='number' min='0' step='0' placeholder='비용' />
+                    <input type='number' min='0' step='0' placeholder='비용' onChange={amountChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     {/* <label>날짜</label> */}
-                    <input type='date' min='2019-01-01' max='2025-12-31' />
+                    <input type='date' min='2019-01-01' max='2025-12-31' onChange={dateChangeHandler} />
                 </div>
             </div>
             <div className='new-expense__actions'>
