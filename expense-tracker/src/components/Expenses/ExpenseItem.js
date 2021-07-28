6f9d0acb16e13fc1b16bb00e26815fc1ed3e1608
Glenn <a810.glenn@gmail.com>
2021-07-28 22:56:21 +0900

    stateless component 예시

--- expense-tracker/src/components/Expenses/ExpenseItem.js
+++ expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -4,24 +4,25 @@ import Card from '../UI/Card'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title)
-    console.log('ExpenseItem evaluated by React')
+    // const [title, setTitle] = useState(props.title)
+    // console.log('ExpenseItem evaluated by React')
 
     // let title = props.title
 
-    const clickHandler = () => {
-        setTitle ('편집 상태')
-        console.log(title)
-    }
+    // const clickHandler = () => {
+    //     setTitle ('편집 상태')
+    //     console.log(title)
+    // }
 
     return(
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                {/* <h2>{title}</h2> */}
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount} 원</div>
             </div>
-            <button onClick={clickHandler}>수정</button>
+            {/* <button onClick={clickHandler}>수정</button> */}
         </Card>
     )
 }
