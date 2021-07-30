7652a62394c6e8793b768206a99df79d65ba7fe4
Glenn <a810.glenn@gmail.com>
2021-07-30 21:56:31 +0900

    prev snapshot 컨트롤 복습, state를 통한 item추가 복습

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,42 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Expenses from './components/Expenses/Expenses'
 import NewExpense from './components/Form/NewExpense'
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: '자동차 보험',
+    amount: 294670,
+    date: new Date(2021, 6, 6)
+  },
+  {
+    id: 'e2',
+    title: '냥놈들 사료',
+    amount: 26000,
+    date: new Date(2021, 5, 21)
+  },
+  {
+    id: 'e3',
+    title: '오드 면역억제제',
+    amount: 180000,
+    date: new Date(2021, 5, 16)
+  },
+  {
+    id: 'e4',
+    title: '쿠팡 생필품',
+    amount: 36700,
+    date: new Date(2021, 6, 4)
+  }
+]
 
 const App = () => {
   // regular JavaScript, cumbersome
   // const para = document.createElement('p')
   // para.textContent = 'This is also visible'
   // document.getElementById('root').append(para)
-  const expenses = [
-    {
-      id: 'e1',
-      title: '자동차 보험',
-      amount: 294670,
-      date: new Date(2021, 6, 6)
-    },
-    {
-      id: 'e2',
-      title: '냥놈들 사료',
-      amount: 26000,
-      date: new Date(2021, 5, 21)
-    },
-    {
-      id: 'e3',
-      title: '오드 면역억제제',
-      amount: 180000,
-      date: new Date(2021, 5, 16)
-    },
-    {
-      id: 'e4',
-      title: '쿠팡 생필품',
-      amount: 36700,
-      date: new Date(2021, 6, 4)
-    }
-  ]
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
 
   const addExpenseHandler  = expense => {
-    console.log('In App.js')
-    console.log(expense)
+    // console.log('In App.js')
+    // console.log(expense)
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    })
   }
 
   // return React.createElement(
