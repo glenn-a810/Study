492d19d07b64284560182c4d385a77cc629e1759
Glenn <a810.glenn@gmail.com>
2021-08-07 22:16:01 +0900

    state, props 가져와서 chart로 표시. 뭐하는지 모르겠음..

--- expense-tracker/src/components/Expenses/Expenses.js
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from '../UI/Card'
 import ExpenseFilter from './ExpenseFilter'
 import './Expenses.css'
 import ExpenseList from './ExpenseList'
+import ExpenseChart from './ExpenseChart'
 
 const Expenses = props => {
     const [filteredYear, setFilteredYear] = useState('2021')
@@ -24,6 +25,7 @@ const Expenses = props => {
                     selected={filteredYear} 
                     onFilterChange={filterChangeHandler} 
                 />
+                <ExpenseChart expenses={selectedYearFilter} />
                 <ExpenseList items={selectedYearFilter} />
                 {/* {expensesContent} */}
                 {/* {selectedYearFilter.length === 0 && <p>No items found.</p>}
