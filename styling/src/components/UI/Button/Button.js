2f446968202ed690493a7c077b8552b5241074e0
Glenn <a810.glenn@gmail.com>
2021-08-11 22:51:00 +0900

    styled component 연습. 왜 쓰는지 다시 복습

--- styling/src/components/UI/Button/Button.js
+++ styling/src/components/UI/Button/Button.js
@@ -1,13 +1,35 @@
-import React from 'react';
+// import React from 'react';
+import styled from 'styled-components';
 
-import './Button.css';
+// import './Button.css';
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #3d70b2;
+  color: White;
+  background: #3d70b2;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #3166ac;
+    border-color: #3166ac;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`
+
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
