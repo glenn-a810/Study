3a4ceeb7b824671279fc9a9c764098d010befb57
Glenn <a810.glenn@gmail.com>
2021-08-15 21:43:39 +0900

    CSS Modules 사용해보기. 이건 좀 헷갈리는듯..

--- styling/src/components/CourseGoals/CourseInput/CourseInput.js
+++ styling/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -2,34 +2,34 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${props => props.invalid ? 'red' : 'black'};
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${props => props.invalid ? 'red' : 'black'};
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    /* border: 1px solid #ccc; */
-    border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
-    background: ${props => props.invalid ? '#ffd7d7' : 'transparent'};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     /* border: 1px solid #ccc; */
+//     border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
+//     background: ${props => props.invalid ? '#ffd7d7' : 'transparent'};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #c5dbf8;
-    border-color: #3d70b2;
-  }
+//   & input:focus {
+//     outline: none;
+//     background: #c5dbf8;
+//     border-color: #3d70b2;
+//   }
 
   /* &.invalid input {
     border-color: red;
@@ -39,7 +39,7 @@ const FormControl = styled.div`
   &.invalid label {
     color: red;
   } */
-`
+// `
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -69,10 +69,10 @@ const CourseInput = props => {
           style={{borderColor: !isValid ? 'red' : '#ccc', backgroundColor: !isValid ? '#ffdada' : 'transparent'}} 
           type="text" onChange={goalInputChangeHandler} 
         /> */}
-      <FormControl invalid={!isValid}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Corse Goal</label>
         <input type='text' onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       {/* </div> */}
       <Button type="submit">Add Goal</Button>
     </form>
