050870aafe58864a73720318a937a72d6897a17f
Glenn <a810.glenn@gmail.com>
2021-08-25 22:39:20 +0900

    userData를 map으로 일단 나열하기

--- practice/src/components/UI/Card.js
+++ practice/src/components/UI/Card.js
@@ -3,18 +3,28 @@ import React from 'react'
 const Card = () => {
     const userData = [
         {
-            userName : '루이',
-            userAge : 11
+            userName: '루이',
+            userAge: 11
         },
         {
-            userName : '오드',
-            userAge : 10
+            userName: '오드',
+            userAge: 10
+        },
+        {
+            userName: '하루',
+            userAge: 1
         }
     ]
 
     return(
+        // <div>
+        //     {userData[0].userName}
+        //     {userData[0].userAge}
+        // </div>
         <div>
-            {userData[0].userName}
+            {userData.map(userList => (
+                <p>{userList.userName} {userList.userAge}</p>
+            ))}
         </div>
     )
 }
