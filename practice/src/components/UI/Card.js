f0df4b5008ec6d1330b795a030998d44a7f4a156
Glenn <a810.glenn@gmail.com>
2021-08-26 21:43:21 +0900

    map에 id값을 넣긴 넣었는데.. key값을 넣어야하네..

--- practice/src/components/UI/Card.js
+++ practice/src/components/UI/Card.js
@@ -3,14 +3,17 @@ import React from 'react'
 const Card = () => {
     const userData = [
         {
+            id: 1,
             userName: '루이',
             userAge: 11
         },
         {
+            id: 2,
             userName: '오드',
             userAge: 10
         },
         {
+            id: 3,
             userName: '하루',
             userAge: 1
         }
@@ -23,7 +26,7 @@ const Card = () => {
         // </div>
         <div>
             {userData.map(userList => (
-                <p>{userList.userName} {userList.userAge}</p>
+                <p>{userList.id} {userList.userName} {userList.userAge}</p>
             ))}
         </div>
     )
